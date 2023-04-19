41df6829b63a03d0f8824aab505e30beb11e0701
V.O.T <makluganteng11@gmail.com>
2023-04-19 09:23:02 +0800

    First circuit

---
+++ Chapter-1/src/main.rs
@@ -0,0 +1,32 @@
+use halo2_proofs::{arithmetic::Field, circuit::{Chip, Layouter, Value}, plonk::Error};
+
+trait NumericInstructions<F: Field>: Chip<F> {
+    /// Variable representing a number.
+    type Num;
+
+    /// Loads a number into the circuit as a private input.
+    fn load_private(&self, layouter: impl Layouter<F>, a: Value<F>) -> Result<Self::Num, Error>;
+
+    /// Loads a number into the circuit as a fixed constant.
+    fn load_constant(&self, layouter: impl Layouter<F>, constant: F) -> Result<Self::Num, Error>;
+
+    /// Returns `c = a * b`.
+    fn mul(
+        &self,
+        layouter: impl Layouter<F>,
+        a: Self::Num,
+        b: Self::Num,
+    ) -> Result<Self::Num, Error>;
+
+    /// Exposes a number as a public input to the circuit.
+    fn expose_public(
+        &self,
+        layouter: impl Layouter<F>,
+        num: Self::Num,
+        row: usize,
+    ) -> Result<(), Error>;
+}
+
+fn main() {
+    println!("Hello, world!");
+}
